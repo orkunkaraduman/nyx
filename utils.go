dd6869a9d5dae86ab9d5b0c002ae8bf2b0af3ddd
Orkun karaduman <orkunkaraduman@gmail.com>
2018-02-17 11:43:43 +0300

    disabled error returning at isPrivateHostname

--- utils.go
+++ utils.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"errors"
 	"net"
 )
 
-func isPrivateHostname(hostname string) (bool, error) {
+func isPrivateHostname(hostname string) bool {
 	_, localBitBlock, _ := net.ParseCIDR("127.0.0.0/8")
 	_, private24BitBlock, _ := net.ParseCIDR("10.0.0.0/8")
 	_, private20BitBlock, _ := net.ParseCIDR("172.16.0.0/12")
@@ -14,17 +13,17 @@ func isPrivateHostname(hostname string) (bool, error) {
 	_, privateIPv6, _ := net.ParseCIDR("fd00::/8")
 	addrs, err := net.LookupHost(hostname)
 	if err != nil {
-		return false, err
+		return false
 	}
 	for _, addr := range addrs {
 		ip := net.ParseIP(addr)
 		if ip == nil {
-			return false, errors.New("Invalid IP")
+			return false
 		}
 		if localBitBlock.Contains(ip) || localBitBlockIPv6.Contains(ip) || private24BitBlock.Contains(ip) || private20BitBlock.Contains(ip) ||
 			private16BitBlock.Contains(ip) || privateIPv6.Contains(ip) {
-			return true, nil
+			return true
 		}
 	}
-	return false, nil
+	return false
 }
